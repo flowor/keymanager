f6f0de87afdcecbf4c05a116475c0e969b4fc2b0
Timothy Rose <trose2@umassd.edu>
2019-05-31 20:44:00 -0400

    initial commit

---
+++ Key Manager/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Key_Manager
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.listKeys = new System.Windows.Forms.ListView();
+            this.colRoomSpace = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colKeyCodeRm = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colKeyCodeApt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colStudentId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNameLast = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNameFirst = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDateStamp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newEntryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setServerLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.createDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listKeys
+            // 
+            this.listKeys.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colRoomSpace,
+            this.colKeyCodeRm,
+            this.colKeyCodeApt,
+            this.colStudentId,
+            this.colNameLast,
+            this.colNameFirst,
+            this.colDateStamp});
+            this.listKeys.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listKeys.FullRowSelect = true;
+            this.listKeys.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.listKeys.Location = new System.Drawing.Point(0, 24);
+            this.listKeys.Name = "listKeys";
+            this.listKeys.Size = new System.Drawing.Size(762, 505);
+            this.listKeys.TabIndex = 1;
+            this.listKeys.UseCompatibleStateImageBehavior = false;
+            this.listKeys.View = System.Windows.Forms.View.Details;
+            // 
+            // colRoomSpace
+            // 
+            this.colRoomSpace.Text = "Room Space";
+            this.colRoomSpace.Width = 109;
+            // 
+            // colKeyCodeRm
+            // 
+            this.colKeyCodeRm.Text = "Key Code (Room)";
+            this.colKeyCodeRm.Width = 111;
+            // 
+            // colKeyCodeApt
+            // 
+            this.colKeyCodeApt.Text = "Key Code (APT)";
+            this.colKeyCodeApt.Width = 92;
+            // 
+            // colStudentId
+            // 
+            this.colStudentId.Text = "Student ID";
+            this.colStudentId.Width = 99;
+            // 
+            // colNameLast
+            // 
+            this.colNameLast.Text = "Last Name";
+            this.colNameLast.Width = 102;
+            // 
+            // colNameFirst
+            // 
+            this.colNameFirst.Text = "First Name";
+            this.colNameFirst.Width = 96;
+            // 
+            // colDateStamp
+            // 
+            this.colDateStamp.Text = "Date Last Updated";
+            this.colDateStamp.Width = 105;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(762, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newEntryToolStripMenuItem,
+            this.importCSVToolStripMenuItem,
+            this.exportCSVToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newEntryToolStripMenuItem
+            // 
+            this.newEntryToolStripMenuItem.Name = "newEntryToolStripMenuItem";
+            this.newEntryToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.newEntryToolStripMenuItem.Text = "New Entry";
+            // 
+            // importCSVToolStripMenuItem
+            // 
+            this.importCSVToolStripMenuItem.Name = "importCSVToolStripMenuItem";
+            this.importCSVToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.importCSVToolStripMenuItem.Text = "Import Keys";
+            this.importCSVToolStripMenuItem.Click += new System.EventHandler(this.ImportCSVToolStripMenuItem_Click);
+            // 
+            // exportCSVToolStripMenuItem
+            // 
+            this.exportCSVToolStripMenuItem.Name = "exportCSVToolStripMenuItem";
+            this.exportCSVToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.exportCSVToolStripMenuItem.Text = "Export Keys";
+            this.exportCSVToolStripMenuItem.Click += new System.EventHandler(this.ExportCSVToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setServerLocationToolStripMenuItem,
+            this.createDatabaseToolStripMenuItem,
+            this.colorsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // setServerLocationToolStripMenuItem
+            // 
+            this.setServerLocationToolStripMenuItem.Name = "setServerLocationToolStripMenuItem";
+            this.setServerLocationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setServerLocationToolStripMenuItem.Text = "Select Database";
+            this.setServerLocationToolStripMenuItem.Click += new System.EventHandler(this.SetServerLocationToolStripMenuItem_Click);
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 506);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(762, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // createDatabaseToolStripMenuItem
+            // 
+            this.createDatabaseToolStripMenuItem.Name = "createDatabaseToolStripMenuItem";
+            this.createDatabaseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createDatabaseToolStripMenuItem.Text = "Create Database";
+            this.createDatabaseToolStripMenuItem.Click += new System.EventHandler(this.CreateDatabaseToolStripMenuItem_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 529);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listKeys);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "Spare Key Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listKeys;
+        private System.Windows.Forms.ColumnHeader colRoomSpace;
+        private System.Windows.Forms.ColumnHeader colKeyCodeRm;
+        private System.Windows.Forms.ColumnHeader colKeyCodeApt;
+        private System.Windows.Forms.ColumnHeader colStudentId;
+        private System.Windows.Forms.ColumnHeader colNameFirst;
+        private System.Windows.Forms.ColumnHeader colDateStamp;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newEntryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importCSVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportCSVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setServerLocationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader colNameLast;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem createDatabaseToolStripMenuItem;
+    }
+}
+
---
+++ Key Manager/Form1.cs
@@ -0,0 +1,413 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Key_Manager
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            listKeys.Resize += new EventHandler(listKeys_onResize);
+            listKeys.ColumnClick += new ColumnClickEventHandler(listKeys_ColumnClick);
+            listKeys.DoubleClick += new EventHandler(listKeys_DoubleClick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResizeListViewColumns(listKeys);
+
+            List<ListViewItem> listViewItems = new List<ListViewItem>();
+
+            listViewItems.Add(new ListViewItem(new string[] {
+                "HK-3101",
+                "31756",
+                "8-1",
+                "01685290",
+                "Rose",
+                "Timothy",
+                "5/25/2019"
+            }, -1, Color.Empty, Properties.Settings.Default.colorDefault, null));
+
+            listViewItems.Add(new ListViewItem(new string[] {
+                "HK-3102",
+                "31757",
+                "8-1",
+                "01685260",
+                "Toes",
+                "Jimothy",
+                "5/26/2019"
+            }, -1, Color.Empty, Properties.Settings.Default.colorDefault, null));
+
+            listViewItems.Add(new ListViewItem(new string[] {
+                "HK-3103",
+                "31758",
+                "8-1",
+                "01685160",
+                "Joseph",
+                "Jerry",
+                "5/26/2019"
+            }, -1, Color.Empty, Properties.Settings.Default.colorDefault, null));
+
+            listViewItems.Add(new ListViewItem(new string[] {
+                "HK-3104",
+                "31759",
+                "8-1",
+                "01685110",
+                "Rohrer",
+                "Brad",
+                "5/26/2019"
+            }, -1, Color.Empty, Properties.Settings.Default.colorAptOut, null));
+
+
+            listKeys.Items.AddRange(listViewItems.ToArray());
+            rePopulateListFromDB(listKeys);
+        }
+
+        private void rePopulateListFromDB(ListView lv)
+        {
+            lv.Items.Clear();
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + Properties.Settings.Default.dbPath + ";Version=3;");
+
+
+            try
+            {
+                m_dbConnection.Open();
+                string sql = "SELECT * FROM keylist order by RoomSpace asc";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ListViewItem item = new ListViewItem(reader["RoomSpace"].ToString());
+                    item.SubItems.Add(reader["KeyCodeRm"].ToString());
+                    item.SubItems.Add(reader["KeyCodeApt"].ToString());
+                    item.SubItems.Add(reader["StudentId"].ToString());
+                    item.SubItems.Add(reader["NameLast"].ToString());
+                    item.SubItems.Add(reader["NameFirst"].ToString());
+                    item.SubItems.Add(reader["DateStamp"].ToString());
+
+                    switch(reader.GetInt32(reader.GetOrdinal("status")))
+                    {
+                        case 0:
+                            item.BackColor = Properties.Settings.Default.colorDefault;
+                            break;
+                        case 1:
+                            item.BackColor = Properties.Settings.Default.colorAptOut;
+                            break;
+                        case 2:
+                            item.BackColor = Properties.Settings.Default.colorRmOut;
+                            break;
+                    }
+
+                    lv.Items.Add(item);
+                }
+
+
+                m_dbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void listKeys_onResize(object sender, EventArgs e)
+        {
+            ResizeListViewColumns(listKeys);
+        }
+
+        private void ResizeListViewColumns(ListView lv)
+        {
+            lv.Columns[lv.Columns.Count - 1].Width = -2;
+        }
+
+        // The column we are currently using for sorting.
+        private ColumnHeader SortingColumn = null;
+        
+        // csharphelper.com/blog/2014/09/sort-a-listview-using-the-column-you-click-in-c
+        // Sort on this column.
+        private void listKeys_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Get the new sorting column.
+            ColumnHeader new_sorting_column = listKeys.Columns[e.Column];
+
+            // Figure out the new sorting order.
+            System.Windows.Forms.SortOrder sort_order;
+            if (SortingColumn == null)
+            {
+                // New column. Sort ascending.
+                sort_order = SortOrder.Ascending;
+            }
+            else
+            {
+                // See if this is the same column.
+                if (new_sorting_column == SortingColumn)
+                {
+                    // Same column. Switch the sort order.
+                    if (SortingColumn.Text.StartsWith("∧ "))
+                    {
+                        sort_order = SortOrder.Descending;
+                    }
+                    else
+                    {
+                        sort_order = SortOrder.Ascending;
+                    }
+                }
+                else
+                {
+                    // New column. Sort ascending.
+                    sort_order = SortOrder.Ascending;
+                }
+
+                // Remove the old sort indicator.
+                SortingColumn.Text = SortingColumn.Text.Substring(2);
+            }
+
+            // Display the new sort order.
+            SortingColumn = new_sorting_column;
+            if (sort_order == SortOrder.Ascending)
+            {
+                SortingColumn.Text = "∧ " + SortingColumn.Text;
+            }
+            else
+            {
+                SortingColumn.Text = "∨ " + SortingColumn.Text;
+            }
+
+            // Create a comparer.
+            listKeys.ListViewItemSorter =
+                new ListViewComparer(e.Column, sort_order);
+
+            // Sort.
+            listKeys.Sort();
+        }
+
+        private void listKeys_DoubleClick(object sender, EventArgs e)
+        {
+            if (listKeys.SelectedItems != null)
+            {
+                MessageBox.Show(listKeys.SelectedItems[0].Text);
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+        private async void ExportCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Save File in Excel (.xlsx format)
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Excel Workbook|*.xlsx";
+            saveDlg.RestoreDirectory = true;
+            saveDlg.Title = "Export keylist as an Excel File";
+            saveDlg.DefaultExt = "xlsx";
+            saveDlg.AddExtension = true;
+
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+
+                progressBar1.Visible = true;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+
+                // simply start and await the loading task
+                await Task.Run(() => exportDB(saveDlg.FileName));
+
+                // re-enable things
+                progressBar1.Visible = false;
+            }
+        }
+
+        private async void ImportCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Are you sure you want to delete the current existing keylist and replace it with the one you are importing?", "Reset Warning", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+                return;
+            }
+
+            // Open Excel File (.xlsx format)
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "Excel Workbook|*.xlsx";
+            openDlg.RestoreDirectory = true;
+            openDlg.Title = "Import excel file into keylist";
+
+            // Show the open dialog
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+
+
+                progressBar1.Visible = true;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+
+                // simply start and await the loading task
+                await Task.Run(() => importDB(openDlg.FileName));
+
+                rePopulateListFromDB(listKeys);
+
+                // re-enable things
+                progressBar1.Visible = false;
+            }
+        }
+
+        private void exportDB(String path)
+        {
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + Properties.Settings.Default.dbPath + ";Version=3;");
+
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Add(1);
+
+                try
+                {
+
+                    Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets[1];
+
+                    // Columns B-F Should be formatted as Text
+                    ws.get_Range("B1").EntireColumn.NumberFormat = "@";
+                    ws.get_Range("C1").EntireColumn.NumberFormat = "@";
+                    ws.get_Range("D1").EntireColumn.NumberFormat = "@";
+
+                    int r, c;
+                    c = 1;
+                    r = 2; // Start At Second Row for headers
+
+                    string[] headers = { "Room Space", "Key Code (Room)", "Key Code (APT)", "Student ID", "Last Name", "First Name", "Date Last Updated", "Status" };
+
+                    foreach (string header in headers)
+                        ws.Cells[1, c++] = header;
+
+                    c = 1; // Reset Column
+
+
+
+                    m_dbConnection.Open();
+                    string sql = "SELECT * FROM keylist order by RoomSpace asc";
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ws.Cells[r, 1] = reader["RoomSpace"].ToString();
+                        ws.Cells[r, 2] = reader["KeyCodeRm"].ToString();
+                        ws.Cells[r, 3] = reader["KeyCodeApt"].ToString();
+                        ws.Cells[r, 4] = reader["StudentId"].ToString();
+                        ws.Cells[r, 5] = reader["NameLast"].ToString();
+                        ws.Cells[r, 6] = reader["NameFirst"].ToString();
+                        ws.Cells[r, 7] = reader["DateStamp"].ToString();
+                        ws.Cells[r, 8] = reader["status"].ToString();
+                        r++;
+                    }
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+                finally
+                {
+                    m_dbConnection.Close();
+
+                    wb.SaveAs(path);
+                    wb.Close(false, Type.Missing, Type.Missing);
+                    app.Quit();
+                }
+        }
+
+        private void importDB(String path) {
+
+                SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + Properties.Settings.Default.dbPath + ";Version=3;");
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Open(path);
+
+                try
+                {
+
+                    Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets[1];
+                    Microsoft.Office.Interop.Excel.Range range = (Microsoft.Office.Interop.Excel.Range)ws.UsedRange;
+
+                    int rowCount = range.Rows.Count;
+                    int colCount = range.Columns.Count;
+
+                    m_dbConnection.Open();
+
+                    // Empty Current Keylist
+                    string sql = "DELETE FROM keylist";
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    command.ExecuteNonQuery();
+
+
+                    command = new SQLiteCommand("INSERT INTO keylist VALUES (?,?,?,?,?,?,?)", m_dbConnection);
+
+                    for (int r = 2; r <= rowCount; r++)
+                    {
+
+                        command = new SQLiteCommand("INSERT INTO keylist VALUES (@RoomSpace,@KeyCodeRM,@KeyCodeAPT,@StudentID,@LastName,@FirstName,@DateStamp,@status)", m_dbConnection);
+                        command.Parameters.AddWithValue("@RoomSpace", range.Cells[r, 1].Value2.ToString());
+                        command.Parameters.AddWithValue("@KeyCodeRM", range.Cells[r, 2].Value2.ToString());
+                        command.Parameters.AddWithValue("@KeyCodeAPT", range.Cells[r, 3].Value2.ToString());
+                        command.Parameters.AddWithValue("@StudentID", range.Cells[r, 4].Value2.ToString());
+                        command.Parameters.AddWithValue("@LastName", range.Cells[r, 5].Value2.ToString());
+                        command.Parameters.AddWithValue("@FirstName", range.Cells[r, 6].Value2.ToString());
+
+                        command.Parameters.AddWithValue("@DateStamp", DateTime.FromOADate(double.Parse(range.Cells[r, 7].Value2.ToString())).ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                        command.Parameters.AddWithValue("@status", range.Cells[r, 8].Value2.ToString());
+
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+                finally
+                {
+                    m_dbConnection.Close();
+
+                    wb.Close(false, Type.Missing, Type.Missing);
+                    app.Quit();
+                }
+            
+        }
+
+        private void SetServerLocationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "sqlite database|*.sqlite";
+            openDlg.RestoreDirectory = true;
+            openDlg.Title = "Select Database";
+
+            if (openDlg.ShowDialog() == DialogResult.OK && openDlg.FileName != "")
+            {
+
+                // Try to connect and check if keylist table exists. If not, error, otherwise update propreties
+
+                Properties.Settings.Default.dbPath = openDlg.FileName;
+                MessageBox.Show("Please restart application to view changes", "Updated Database");
+            }
+
+        }
+
+        private void CreateDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
